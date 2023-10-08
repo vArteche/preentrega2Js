bd06f4d7dd937d20e6a26ce50cd0944fa0bf03cd
victoriaArteche <agostinavictoria4@gmail.com>
2023-10-08 17:52:40 -0300

    faltan estilos y total del carrito

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-//PRODUCTOS
+document.addEventListener("DOMContentLoaded", function() {
+  //PRODUCTOS
 // CONSTRUCTOR DE PRODUCTOS
 class nuevoProducto {
   constructor(id, nombre, precio, descripcion) {
-    this.id=id;
-    // this.img = img;
+    this.id = id;
     this.nombre = nombre;
     this.descripcion = descripcion;
     this.precio = "$" + precio;
-  };
+    this.cantidad = 1; 
+  }
 }
 
 // PRODUCTOS DISPONIBLES
@@ -19,49 +20,84 @@ const PRODUCTO5 = new nuevoProducto(5, "LECHE", 900, 'La Serenísima 3% - 1lt');
 
 const productos = [PRODUCTO1, PRODUCTO2, PRODUCTO3, PRODUCTO4, PRODUCTO5];
 
-
 //DOM
-const cardBody = document.querySelector(".card-body");
+const cardBody = document.getElementById("card-body");
 
 //LISTA DE PRODUCTOS
 for (const producto of productos){
   const div = document.createElement('div');
   div.innerHTML = `
-  <div class = "card-product">
-      <div class="card-content">
-      <div class="imgProducto">
-      <img  src="${producto.img}">
-      </div>
-        <span class="card-title">${producto.nombre}</span>
-          <p>${producto.descripcion}</p>
-          <p>${producto.precio}</p>
-          <button class="agregar" id="${producto.id}">Agregar al carrito</button>
-      </div>
-    `;
-  cardBody.appendChild(div);
-};
-//AGREGAR AL CARRITO
-const carrito = document.getElementById("carrito");
-
-for(const producto of productos){
-  const botonAgregado = document.getElementById(`${producto.id}`);
-botonAgregado.addEventListener('click', (e)=>{
-  let divCarrito = document.createElement("div");
-  divCarrito.innerHTML = `
     <div class="card-product">
       <div class="card-content">
         <div class="imgProducto">
-          <img src="${producto.img}">
         </div>
         <span class="card-title">${producto.nombre}</span>
         <p>${producto.descripcion}</p>
         <p>${producto.precio}</p>
-        <p> Cantidad: </p>
+        <button class="agregado" id="${producto.id}">Agregar al carrito</button>
       </div>
     </div>
   `;
-  document.appendChild(divCarrito);
-});
+  cardBody.appendChild(div);
+}
+
+//AGREGAR AL CARRITO
+const carritoDom = document.getElementById("carrito");
+const carrito = [];
+
+// Función para agregar al carrito
+function agregarAlCarrito(id) {
+  const existe = carrito.some(producto => producto.id === parseInt(id));
+
+  if (existe) {
+    carrito.map(producto => {
+      if (producto.id === parseInt(id)) {
+        producto.cantidad += 1;
+      }
+    });
+  } else {
+    let productoEncontrado = productos.find(producto => producto.id === parseInt(id));
+    carrito.push(productoEncontrado);
+  }
+
+  // Actualizar el contenido del carrito
+  if (carritoDom) {
+    carritoDom.innerHTML = "";
+    let total = 0;
+  
+    carrito.forEach(producto => {
+      let divCarrito = document.createElement('div');
+      divCarrito.innerHTML = `
+        <div class="card-product">
+          <div class="card-content">
+            <div class="imgProducto">
+              <img src="">
+            </div>
+            <span class="card-title">${producto.nombre}</span>
+            <p>${producto.descripcion}</p>
+            <p>${producto.precio}</p>
+            <p>Cantidad: ${producto.cantidad}</p>
+          </div>
+        </div>
+      `;
+      carritoDom.appendChild(divCarrito);
+  
+      total += producto.precio * producto.cantidad;
+      let divTotal = document.createElement('div');
+      divTotal.innerHTML = `Total: $${total}`;
+      carritoDom.appendChild(divTotal);
+    });
+  } else {
+    console.error("El elemento con ID 'carrito' no existe en el DOM");
+  }
 };
 
-
+const btnAgregado = document.querySelectorAll(".agregado");
+btnAgregado.forEach((elegido) => {
+  elegido.addEventListener('click', (e) => {
+    // Obtener el id del producto del botón
+    const idProducto = e.target.id;
+    // Llamar a la función agregarAlCarrito con el id del producto
+    agregarAlCarrito(idProducto);
+  });
+})});
