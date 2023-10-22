89d41a2a39c9bdb2adba5bc23848aa11a2fb07ad
victoriaArteche <agostinavictoria4@gmail.com>
2023-10-22 02:19:34 -0300

    estilos, imagenes faltan funciones

--- main.js
+++ main.js
@@ -7,20 +7,21 @@ document.addEventListener("DOMContentLoaded", function() {
     //PRODUCTOS
   // CONSTRUCTOR DE PRODUCTOS
   class nuevoProducto {
-    constructor(id, nombre, precio, descripcion) {
+    constructor(id, nombre, precio, descripcion, img) {
       this.id = id;
       this.nombre = nombre;
       this.descripcion = descripcion;
       this.precio = precio;
       this.cantidad = 1;
+      this.img = img;
     }
   }
   // PRODUCTOS DISPONIBLES
-  const PRODUCTO1 = new nuevoProducto(1, "YERBA MATE", 1200, 'Mañanita - 1kg');
-  const PRODUCTO2 = new nuevoProducto(2, "ARROZ", 600, 'Doble Carolina - 1kg')
-  const PRODUCTO3 = new nuevoProducto(3, "JABON LIQUIDO", 1800, 'Skip - 5lts');
-  const PRODUCTO4 = new nuevoProducto(4, "ACEITE", 1000, 'Natura - 900ml');
-  const PRODUCTO5 = new nuevoProducto(5, "LECHE", 900, 'La Serenísima 3% - 1lt');
+  const PRODUCTO1 = new nuevoProducto(1, "YERBA MATE", 1200, 'Mañanita - 1kg', "./assets/img/yerbanueva.jpg");
+  const PRODUCTO2 = new nuevoProducto(2, "ARROZ", 600, 'Doble Carolina - 1kg', "./assets/img/arroznueva.jpg")
+  const PRODUCTO3 = new nuevoProducto(3, "JABON LIQUIDO", 1800, 'Skip - 5lts', "./assets/img/jabonnueva.jpg");
+  const PRODUCTO4 = new nuevoProducto(4, "ACEITE", 1000, 'Natura - 900ml', "./assets/img/aceitenueva.jpg");
+  const PRODUCTO5 = new nuevoProducto(5, "LECHE", 900, 'La Serenísima 3% - 1lt', "./assets/img/lechenueva.jpg");
 
   const productos = [PRODUCTO1, PRODUCTO2, PRODUCTO3, PRODUCTO4, PRODUCTO5];
 
@@ -34,6 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
       <div class="card-product">
         <div class="card-content">
           <div class="imgProducto">
+          <img src=${producto.img}>
           </div>
           <span class="card-title">${producto.nombre}</span>
           <p>${producto.descripcion}</p>
@@ -52,6 +54,15 @@ document.addEventListener("DOMContentLoaded", function() {
       const idProducto = e.target.id;
       // Llamar a la función agregarAlCarrito con el id del producto
       agregarAlCarrito(idProducto);
+      Toastify({
+
+        text:"Se añadió 1 producto al carrito",
+        style: {
+          background: "#A7D397"
+        },
+        duration: 3000
+        
+        }).showToast();
     })});
 
     const btnEliminar = document.querySelectorAll(".eliminar");
@@ -77,21 +88,19 @@ document.addEventListener("DOMContentLoaded", function() {
       carrito.push(productoEncontrado);
     };
     renderizarCarrito();
+  
   }
   function eliminarDelCarrito(id) {
-    const existe = carrito.some(producto => producto.id === parseInt(id));
-    if (existe) {
-      carrito.map(producto => {
-        if (producto.id === parseInt(id)) {
-          producto.cantidad -= 1;
-        }
-          return producto;
-      });
-    } else {
-          let productoEncontrado = productos.find(producto => producto.id === parseInt(id));
-      carrito.splice(productoEncontrado, 1);
-    };
-    renderizarCarrito();
+    const producto = carrito.find(producto=> producto.id ===parseInt(id));
+    if(producto){
+      if(producto.cantidad >1){
+        producto.cantidad-=1;
+      }else{
+        carrito= carrito.filter(item => item.id !== parseInt(id));
+      }
+      renderizarCarrito();
+    }
+    
   }
   function renderizarCarrito(){
         // Actualizar el contenido del carrito
@@ -103,7 +112,7 @@ document.addEventListener("DOMContentLoaded", function() {
               <div class="card-product">
                 <div class="card-content">
                   <div class="imgProducto">
-                    <img src="">
+                    <img src=${producto.img}>
                   </div>
                   <span class="card-title">${producto.nombre}</span>
                   <p>${producto.descripcion}</p>
@@ -121,4 +130,11 @@ document.addEventListener("DOMContentLoaded", function() {
           carritoDom.appendChild(divTotal);
           // Almacenar en localStorage
           localStorage.setItem('carrito', JSON.stringify(carrito));
-  }})
+  }}
+  
+  
+  const btnVaciarCarrito = document.querySelector("#vaciarCarrito"));
+
+  btnVaciarCarrito.addEventListener(click, (e)=>{
+    
+  })
